3a330befcedaf55586637328692b387269de35e9
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-05-28 03:14:14 +0700

    add log

--- worker.py
+++ worker.py
@@ -1,61 +1,80 @@
 import pyautogui
+
 pyautogui.FAILSAFE = False
 import time
 import random
 import math
 import requests
-
-
+import json
 
 
 def random_move():
-    pyautogui.moveTo(random.choice(range(1, 1920)), random.choice(range(1, 1080)), 2, pyautogui.easeInOutQuad)
+    pyautogui.moveTo(
+        random.choice(range(1, 1920)),
+        random.choice(range(1, 1080)),
+        2,
+        pyautogui.easeInOutQuad,
+    )
     pyautogui.click()
-    time.sleep(random.choice(range(1,3)))
+    time.sleep(random.choice(range(1, 3)))
 
 
 def mount_up():
     pyautogui.press("a")
 
+
 def get_predict(image):
     from io import BytesIO
+
     image_bytes = BytesIO()
-    image.save(image_bytes, format='JPEG', quality=10)
+    image.save(image_bytes, format="JPEG", quality=10)
     image_bytes.seek(0)
-    files = {'file': ('screen.jpg', image_bytes, 'image/jpg')}
+    files = {"file": ("screen.jpg", image_bytes, "image/jpg")}
     headers = {}
     url = "http://detection.diopthe20.com/fiber_detection/"
-    
+
     try:
         response = requests.post(url, files=files, headers=headers)
         return response.json()
-    except requests.exceptions.ConnectionError:
+    except:
         pass
+
 
 def job_request():
     pass
 
-class Worker():
-    master_url = 'http://sv.diopthe20.com/'
 
-    def logging(self):
-        requests.post(self.master_url)
-        
+def log_work(content):
+    url = "http://detection.diopthe20.com/status/"
+
+    payload = json.dumps({"id": 1, "status": "test"})
+    headers = {"Content-Type": "application/json"}
+
+    requests.request("POST", url, headers=headers, data=payload)
+
+
+
 while True:
     image = pyautogui.screenshot()
-    
+
     from timeit import default_timer as timer
+
     t1 = timer()
     result = get_predict(image=image)
     t2 = timer()
-    print("Time taken: ", t2-t1 )
+    print("Time taken: ", t2 - t1)
     print(result)
     if result["data"] != None:
         x1, y1, x2, y2 = result["data"]
-        pyautogui.moveTo(math.ceil((x1+x2)/2), math.ceil((y1+y2)/2), 0.5, pyautogui.easeInOutQuad)
+        pyautogui.moveTo(
+            math.ceil((x1 + x2) / 2),
+            math.ceil((y1 + y2) / 2),
+            0.5,
+            pyautogui.easeInOutQuad,
+        )
         pyautogui.click()
+        log_work("Current work: {} {} {} {}".format(x1, y1, x2, y2))
         time.sleep(2)
     else:
+        log_work("No Fiber Found")
         time.sleep(1)
-    
-
